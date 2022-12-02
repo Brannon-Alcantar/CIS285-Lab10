b3eed7fad55699df2fce56d16657c2cac5ce3512
Brannon Alcantar <brannonalc@gmail.com>
2022-12-01 20:13:02 -0500

    Added test negative and test positive functions

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -7,7 +7,11 @@
 import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
+
 public class testSelectionSort {
+
+	private SelectionSort temp;
+/*
 @Test
 public void test() {
 testPositive();
@@ -15,10 +19,12 @@ testNegative();
 testMixed();
 testDuplicates();
 }
+*/
     public testSelectionSort() {
     }
 
-    public void testPositive(){
+    @Test
+    public void testPositive() {
         int[] arr = new int[5];
         arr[0] = 8;
         arr[1] = 9;
@@ -33,12 +39,64 @@ testDuplicates();
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
 
+        temp = new SelectionSort();
+        int[] selectionSortedArr = temp.basicSelectionSort(arr);
+
+        System.out.println("TEST POSITIVE");
+
+        for (int i = 0; i < selectionSortedArr.length; i++) {
+        	System.out.print(selectionSortedArr[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < Sortedarr.length; i++) {
+        	System.out.print(Sortedarr[i] + " ");
+        }
+
+        System.out.println();
+
         /** add tests to check for this unit test **/
+        assertArrayEquals(Sortedarr, selectionSortedArr);
+
     }
 
-    public void testNegative(){
+    @Test
+    public void testNegative() {
 
         /** Test data contains negative values only **/
+    	int[] arr = new int [5];
+    	arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
+        temp = new SelectionSort();
+        int [] selectionSortedArr = temp.basicSelectionSort(arr);
+
+        System.out.println("TEST NEGATIVE");
+
+        for (int i = 0; i < selectionSortedArr.length; i++) {
+        	System.out.print(selectionSortedArr[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < Sortedarr.length; i++) {
+        	System.out.print(Sortedarr[i] + " ");
+        }
+
+        System.out.println();
+
+        assertArrayEquals(Sortedarr, selectionSortedArr);
     }
 
     public void testMixed(){
